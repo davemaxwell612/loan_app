b0b8b62d04a79fad95f3754d228f364509f1a38b
davemaxwell612 <davemaxwell612@gmail.com>
2022-07-14 22:32:45 +0100

    Loan app Dashboard Layout completed

---
+++ src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<template>
+  <main>
+
+    <Dashboard />
+    
+  </main>
+</template>
+
+<script>
+import Dashboard from '../components/Dashboard.vue'
+  export default {
+    components:{Dashboard}
+  }
+</script>
+
+<style>
+  /* Custom style */
+  .header-right {
+      width: calc(100% - 3.5rem);
+  }
+  .sidebar:hover {
+      width: 16rem;
+  }
+  @media only screen and (min-width: 768px) {
+      .header-right {
+          width: calc(100% - 16rem);
+      }        
+  }
+</style>
